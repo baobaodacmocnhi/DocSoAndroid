8f687efb8e30ab02d98d347529cefce71a9f9d96
BaoBao <baobaodacmocnhi@gmail.com>
2022-02-23 16:37:07 +0700

    update

--- app/src/main/java/vn/com/capnuoctanhoa/docsoandroid/DocSo/ActivityDocSo_GhiChiSo.java
+++ app/src/main/java/vn/com/capnuoctanhoa/docsoandroid/DocSo/ActivityDocSo_GhiChiSo.java
@@ -1,15 +1,107 @@
 package vn.com.capnuoctanhoa.docsoandroid.DocSo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import vn.com.capnuoctanhoa.docsoandroid.Class.CEntityParent;
+import vn.com.capnuoctanhoa.docsoandroid.Class.CLocal;
 import vn.com.capnuoctanhoa.docsoandroid.R;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class ActivityDocSo_GhiChiSo extends AppCompatActivity {
+    private Integer STT = -1;
+    private EditText edtMLT, edtDanhBo, edtHoTen, edtDiaChi, edtDiaChiDHN, edtViTri, edtHieu, edtCo, edtSoThan, edtGB, edtDM, edtDMHN, edtCSM;
+    private Spinner spnCode;
+    private ImageView ivTruoc, ivSau, ivGhiChu, ivIn, ivLuu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_so_ghi_chi_so);
+
+        edtMLT = (EditText) findViewById(R.id.edtMLT);
+        edtDanhBo = (EditText) findViewById(R.id.edtDanhBo);
+        edtHoTen = (EditText) findViewById(R.id.edtHoTen);
+        edtDiaChi = (EditText) findViewById(R.id.edtDiaChi);
+        edtDiaChiDHN = (EditText) findViewById(R.id.edtDiaChiDHN);
+        edtViTri = (EditText) findViewById(R.id.edtViTri);
+        edtHieu = (EditText) findViewById(R.id.edtHieu);
+        edtCo = (EditText) findViewById(R.id.edtCo);
+        edtSoThan = (EditText) findViewById(R.id.edtSoThan);
+        edtGB = (EditText) findViewById(R.id.edtGB);
+        edtDM = (EditText) findViewById(R.id.edtDM);
+        edtDMHN = (EditText) findViewById(R.id.edtDMHN);
+        edtCSM = (EditText) findViewById(R.id.edtCSM);
+        spnCode = (Spinner) findViewById(R.id.spnCode);
+        ivTruoc = (ImageView) findViewById(R.id.ivTruoc);
+        ivSau = (ImageView) findViewById(R.id.ivSau);
+        ivGhiChu = (ImageView) findViewById(R.id.ivGhiChu);
+        ivIn = (ImageView) findViewById(R.id.ivIn);
+        ivLuu = (ImageView) findViewById(R.id.ivLuu);
+
+        ivTruoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (STT > 0) {
+                    STT--;
+                    fillLayout(STT);
+                } else
+                    CLocal.showToastMessage(ActivityDocSo_GhiChiSo.this, "Đầu Danh Sách");
+            }
+        });
+
+        ivSau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (STT < CLocal.listDocSoView.size() - 1) {
+                    STT++;
+                    fillLayout(STT);
+                } else
+                    CLocal.showToastMessage(ActivityDocSo_GhiChiSo.this, "Cuối Danh Sách");
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        try {
+            STT = Integer.parseInt(getIntent().getStringExtra("STT"));
+            if (STT > -1) {
+                fillLayout(STT);
+            }
+        } catch (Exception ex) {
+            CLocal.showToastMessage(ActivityDocSo_GhiChiSo.this, ex.getMessage());
+        }
+    }
+
+    private void fillLayout(final Integer STT) {
+        try {
+            if (CLocal.listDocSoView != null && CLocal.listDocSoView.size() > 0) {
+                ArrayList<String> arrayList = new ArrayList<String>();
+                if (STT >= 0 && STT < CLocal.listDocSoView.size()) {
+                    CEntityParent item = CLocal.listDocSoView.get(STT);
+                    edtMLT.setText(item.getMLT());
+                    edtDanhBo.setText(item.getDanhBo());
+                    edtHoTen.setText(item.getHoTen());
+                    edtDiaChi.setText(item.getDiaChi());
+                    edtDiaChiDHN.setText(item.getDiaChiDHN());
+
+                }
+
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
 }
