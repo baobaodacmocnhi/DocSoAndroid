c6e2c9a3df89c6a2916143c848228bba61de3a2a
BaoBao <baobaodacmocnhi@gmail.com>
2023-03-13 07:49:30 +0700

    update

--- app/src/main/java/vn/com/capnuoctanhoa/docsoandroid/DocSo/ActivityDownDataDocSo.java
+++ app/src/main/java/vn/com/capnuoctanhoa/docsoandroid/DocSo/ActivityDownDataDocSo.java
@@ -361,17 +361,17 @@ public class ActivityDownDataDocSo extends AppCompatActivity {
                             JSONObject jsonObject = jsonResult.getJSONObject(j);
                             CLocal.jsonDocSo.put(jsonObject);
                         }
-                        jsonResult = new JSONArray(ws.getDS_HoaDonTon(spnNam.getSelectedItem().toString(), spnKy.getSelectedItem().toString(), spnDot.getSelectedItem().toString(), (Integer.parseInt(spnID_NhanVien.get(i)) < 10 ? "0" : "") + Integer.parseInt(spnID_NhanVien.get(i))));
-                        for (int j = 0; j < jsonResult.length(); j++) {
-                            JSONObject jsonObject = jsonResult.getJSONObject(j);
-                            CLocal.jsonHoaDonTon.put(jsonObject);
-                        }
+//                        jsonResult = new JSONArray(ws.getDS_HoaDonTon(spnNam.getSelectedItem().toString(), spnKy.getSelectedItem().toString(), spnDot.getSelectedItem().toString(), (Integer.parseInt(spnID_NhanVien.get(i)) < 10 ? "0" : "") + Integer.parseInt(spnID_NhanVien.get(i))));
+//                        for (int j = 0; j < jsonResult.length(); j++) {
+//                            JSONObject jsonObject = jsonResult.getJSONObject(j);
+//                            CLocal.jsonHoaDonTon.put(jsonObject);
+//                        }
                     }
                 } else {
                     if (!Boolean.parseBoolean(ws.checkNgayDoc(spnNam.getSelectedItem().toString(), spnKy.getSelectedItem().toString(), spnDot.getSelectedItem().toString(), selectedMaNV)))
                         return new String[]{"false", "Chưa đến ngày đọc số"};
                     CLocal.jsonDocSo = new JSONArray(ws.getDS_DocSo(spnNam.getSelectedItem().toString(), spnKy.getSelectedItem().toString(), spnDot.getSelectedItem().toString(), selectedMaNV));
-                    CLocal.jsonHoaDonTon = new JSONArray(ws.getDS_HoaDonTon(spnNam.getSelectedItem().toString(), spnKy.getSelectedItem().toString(), spnDot.getSelectedItem().toString(), selectedMaNV));
+//                    CLocal.jsonHoaDonTon = new JSONArray(ws.getDS_HoaDonTon(spnNam.getSelectedItem().toString(), spnKy.getSelectedItem().toString(), spnDot.getSelectedItem().toString(), selectedMaNV));
                 }
 //                if (selectedMaNV.equals("0")) {
 //                    for (int i = 1; i < spnID_NhanVien.size(); i++) {
@@ -488,17 +488,17 @@ public class ActivityDownDataDocSo extends AppCompatActivity {
                         enParent.setPhanMay(jsonObject.getString("PhanMay").replace("null", ""));
                         enParent.setChuBao(Boolean.parseBoolean(jsonObject.getString("ChuBao").replace("null", "")));
                         enParent.setTinhTrang(jsonObject.getString("TinhTrang").replace("null", ""));
-                        if (CLocal.jsonHoaDonTon != null && CLocal.jsonHoaDonTon.length() > 0)
-                            for (int k = 0; k < CLocal.jsonHoaDonTon.length(); k++) {
-                                JSONObject jsonObjectChild = CLocal.jsonHoaDonTon.getJSONObject(k);
-                                if (jsonObjectChild.getString("DanhBo").equals(enParent.getDanhBo().replace(" ", "")) == true) {
-                                    CEntityChild entityChild = new CEntityChild();
-                                    entityChild.setMaHD(jsonObjectChild.getString("MaHD").replace("null", ""));
-                                    entityChild.setKy(jsonObjectChild.getString("KyHD").replace("null", ""));
-                                    entityChild.setTongCong(jsonObjectChild.getString("TongCong").replace("null", ""));
-                                    enParent.getLstHoaDon().add(entityChild);
-                                }
-                            }
+//                        if (CLocal.jsonHoaDonTon != null && CLocal.jsonHoaDonTon.length() > 0)
+//                            for (int k = 0; k < CLocal.jsonHoaDonTon.length(); k++) {
+//                                JSONObject jsonObjectChild = CLocal.jsonHoaDonTon.getJSONObject(k);
+//                                if (jsonObjectChild.getString("DanhBo").equals(enParent.getDanhBo().replace(" ", "")) == true) {
+//                                    CEntityChild entityChild = new CEntityChild();
+//                                    entityChild.setMaHD(jsonObjectChild.getString("MaHD").replace("null", ""));
+//                                    entityChild.setKy(jsonObjectChild.getString("KyHD").replace("null", ""));
+//                                    entityChild.setTongCong(jsonObjectChild.getString("TongCong").replace("null", ""));
+//                                    enParent.getLstHoaDon().add(entityChild);
+//                                }
+//                            }
                         CLocal.listDocSo.add(enParent);
                     }
                     SharedPreferences.Editor editor = CLocal.sharedPreferencesre.edit();
